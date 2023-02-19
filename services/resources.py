a5fc014af38092a4c7eaef4abae27bb6e65d78d8
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-02-20 00:02:18 +0530

    Resource Tweaking

--- services/resources.py
+++ services/resources.py
@@ -1,10 +1,16 @@
 from .models import *
-from import_export import resources
+from django.contrib.auth.models import User
+from import_export import resources,fields,widgets
 class DepartmentResource(resources.ModelResource):
 
     class Meta:
         model = Department
-class ApplicationResource(resources.ModelResource):
 
+class ApplicationResource(resources.ModelResource):
+    user = fields.Field(
+        column_name='Username',
+        attribute='user',
+        widget=widgets.ForeignKeyWidget(User, 'username'))
     class Meta:
         model = Application
+        fields = ['user','department_preferences','linkedin_url','instagram_id','lookup_skills','question_one','question_two' ,'status','date_of_application']
